c333015d89a4c81cae0e628f5b6f7ae6bd48f526
Евгения <69339596+EvgeshaJA@users.noreply.github.com>
2022-03-20 11:42:25 +0300

    changeTitle

--- js/tabs.js
+++ js/tabs.js
@@ -103,3 +103,15 @@ buttons.forEach((i) => {
     });
     });
 });
+
+
+// Поменять title
+
+
+let titleChanger = function(e) {
+    document.title = e.target.innerHTML;
+};
+
+Array.prototype.forEach.call(buttons, function(el) {
+    el.onclick = titleChanger;
+});
